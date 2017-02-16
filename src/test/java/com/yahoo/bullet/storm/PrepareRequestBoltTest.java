bullet-db/bullet-storm

commit 4fb218bb5683eeb8d0a5cd5f29d041a09cf5e2a9

Indentation fix

src/main/java/com/yahoo/bullet/parsing/Aggregation.java
@@ -66,7 +66,7 @@ public class Aggregation implements Configurable, Validatable {
 
     public static final String TYPE_NOT_SUPPORTED_ERROR_PREFIX = "Aggregation type not supported";
     public static final String TYPE_NOT_SUPPORTED_RESOLUTION = "Current supported aggregation types are: RAW, GROUP, " +
-            "                                                   COUNT DISTINCT";
+                                                               "COUNT DISTINCT";
 
     public static final String SUPPORTED_GROUP_OPERATIONS_RESOLUTION =
             "Currently supported operations are: COUNT, AVG, MIN, MAX, SUM";

commit e42ca46f5386bf7e90a3f711ebc6922e54edbc36

Acking and bug fixes: 4fb218b-edc5456

pom.xml
@@ -37,7 +37,7 @@
         <maven.compiler.source>1.8</maven.compiler.source>
         <maven.compiler.target>1.8</maven.compiler.target>
         <storm.version>0.10.2</storm.version>
-        <bullet.record.version>0.0.3</bullet.record.version>
+        <bullet.record.version>0.1.0</bullet.record.version>
         <sketches.version>0.8.3</sketches.version>
     </properties>
 

src/main/java/com/yahoo/bullet/BulletConfig.java
@@ -36,7 +36,7 @@ public class BulletConfig extends Config {
     public static final String AGGREGATION_MAX_SIZE = "rule.aggregation.max.size";
     public static final String AGGREGATION_COMPOSITE_FIELD_SEPARATOR = "rule.aggregation.composite.field.separator";
 
-    public static final String RAW_AGGREGATION_MAX_SIZE = "rule.aggregation.max.size";
+    public static final String RAW_AGGREGATION_MAX_SIZE = "rule.aggregation.raw.max.size";
     public static final String RAW_AGGREGATION_MICRO_BATCH_SIZE = "rule.aggregation.raw.micro.batch.size";
 
     public static final String COUNT_DISTINCT_AGGREGATION_SKETCH_ENTRIES = "rule.aggregation.count.distinct.sketch.entries";

src/main/java/com/yahoo/bullet/Topology.java
@@ -7,6 +7,7 @@ package com.yahoo.bullet;
 
 import com.yahoo.bullet.storm.FilterBolt;
 import com.yahoo.bullet.storm.JoinBolt;
+import com.yahoo.bullet.storm.PrepareRequestBolt;
 import com.yahoo.bullet.storm.TopologyConstants;
 import joptsimple.OptionParser;
 import joptsimple.OptionSet;
@@ -14,7 +15,6 @@ import lombok.extern.slf4j.Slf4j;
 import backtype.storm.Config;
 import backtype.storm.StormSubmitter;
 import backtype.storm.drpc.DRPCSpout;
-import backtype.storm.drpc.PrepareRequest;
 import backtype.storm.drpc.ReturnResults;
 import backtype.storm.topology.IRichSpout;
 import backtype.storm.topology.TopologyBuilder;
@@ -113,7 +113,7 @@ public class Topology {
 
         builder.setSpout(TopologyConstants.DRPC_COMPONENT, new DRPCSpout(function), drpcSpoutParallelism);
 
-        builder.setBolt(TopologyConstants.PREPARE_COMPONENT, new PrepareRequest(), prepareBoltParallelism)
+        builder.setBolt(TopologyConstants.PREPARE_COMPONENT, new PrepareRequestBolt(), prepareBoltParallelism)
                .shuffleGrouping(TopologyConstants.DRPC_COMPONENT);
 
         // Hook in the source of the BulletRecords

src/main/java/com/yahoo/bullet/parsing/Aggregation.java
@@ -76,6 +76,8 @@ public class Aggregation implements Configurable, Validatable {
 
     public static final Error REQUIRES_FIELD_ERROR =
             makeError("This aggregation type requires at least one field", OPERATION_REQUIRES_FIELD_RESOLUTION);
+    public static final Error REQUIRES_FIELD_OR_OPERATION_ERROR =
+            makeError("This aggregation type requires at least one field or operation", "Please add a field or an operation");
 
     public static final Integer DEFAULT_SIZE = 1;
     public static final Integer DEFAULT_MAX_SIZE = 512;
@@ -114,18 +116,24 @@ public class Aggregation implements Configurable, Validatable {
 
     @Override
     public Optional<List<Error>> validate() {
-        if (type == AggregationType.COUNT_DISTINCT) {
-            if (isEmpty(fields)) {
-                return Optional.of(singletonList(REQUIRES_FIELD_ERROR));
-            }
-        }
         // Supported aggregation types should be documented in TYPE_NOT_SUPPORTED_RESOLUTION
         if (!SUPPORTED_AGGREGATION_TYPES.contains(type)) {
             String typeSuffix = type == null ? "" : ": " + type;
             return Optional.of(singletonList(makeError(TYPE_NOT_SUPPORTED_ERROR_PREFIX + typeSuffix,
                                                        TYPE_NOT_SUPPORTED_RESOLUTION)));
         }
-        if (!groupOperations.isEmpty()) {
+        boolean noFields = isEmpty(fields);
+        boolean noOperations = isEmpty(groupOperations);
+        if (noFields) {
+            if (type == AggregationType.COUNT_DISTINCT) {
+                return Optional.of(singletonList(REQUIRES_FIELD_ERROR));
+            }
+            if (type == AggregationType.GROUP && noOperations) {
+                return Optional.of(singletonList(REQUIRES_FIELD_OR_OPERATION_ERROR));
+            }
+        }
+
+        if (!noOperations) {
             for (GroupOperation operation : groupOperations) {
                 if (operation.getField() == null && operation.getType() != GroupOperationType.COUNT) {
                     return Optional.of(singletonList(makeError(GROUP_OPERATION_REQUIRES_FIELD + operation.getType(),

src/main/java/com/yahoo/bullet/storm/PrepareRequestBolt.java
@@ -1,0 +1,63 @@
+/*
+ *  Copyright 2016, Yahoo Inc.
+ *  Licensed under the terms of the Apache License, Version 2.0.
+ *  See the LICENSE file associated with the project for terms.
+ */
+package com.yahoo.bullet.storm;
+
+import backtype.storm.task.OutputCollector;
+import backtype.storm.task.TopologyContext;
+import backtype.storm.topology.OutputFieldsDeclarer;
+import backtype.storm.topology.base.BaseRichBolt;
+import backtype.storm.tuple.Fields;
+import backtype.storm.tuple.Tuple;
+import backtype.storm.tuple.Values;
+import backtype.storm.utils.Utils;
+
+import java.util.Map;
+import java.util.Random;
+
+/**
+ * This class is copied from backtype.storm.drpc.PrepareRequest but does not anchor. This exists so that acking
+ * can be enabled for the topology but not influence DRPC tuple not being acked within the timeout. Since this
+ * will not anchor the DRPC tuple, it will be immediately acked. The downside is that if the tuple is dropped,
+ * there will no longer be a fast-fail mechanism but since bullet-storm depends on arbitrarily long DRPC based
+ * queries, this is a compromise that has one solution.
+ */
+public class PrepareRequestBolt extends BaseRichBolt {
+    public static final String ARGS_STREAM = Utils.DEFAULT_STREAM_ID;
+    public static final String RETURN_STREAM = "ret";
+    public static final String ID_STREAM = "id";
+
+    public static final String REQUEST_FIELD = "request";
+    public static final String RETURN_FIELD = "return";
+    public static final String ARGS_FIELD = "args";
+
+    private OutputCollector collector;
+
+    Random rand;
+
+    @Override
+    public void prepare(Map stormConf, TopologyContext context, OutputCollector collector) {
+        this.collector = collector;
+        rand = new Random();
+    }
+
+    @Override
+    public void execute(Tuple tuple) {
+        String args = tuple.getString(0);
+        String returnInfo = tuple.getString(1);
+        long requestId = rand.nextLong();
+        collector.emit(ARGS_STREAM, new Values(requestId, args));
+        collector.emit(RETURN_STREAM, new Values(requestId, returnInfo));
+        collector.emit(ID_STREAM, new Values(requestId));
+        collector.ack(tuple);
+    }
+
+    @Override
+    public void declareOutputFields(OutputFieldsDeclarer declarer) {
+        declarer.declareStream(ARGS_STREAM, new Fields(REQUEST_FIELD, ARGS_FIELD));
+        declarer.declareStream(RETURN_STREAM, new Fields(REQUEST_FIELD, RETURN_FIELD));
+        declarer.declareStream(ID_STREAM, new Fields(REQUEST_FIELD));
+    }
+}

src/main/java/com/yahoo/bullet/storm/TopologyConstants.java
@@ -7,7 +7,6 @@ package com.yahoo.bullet.storm;
 
 import backtype.storm.Constants;
 import backtype.storm.drpc.DRPCSpout;
-import backtype.storm.drpc.PrepareRequest;
 import backtype.storm.drpc.ReturnResults;
 import backtype.storm.utils.Utils;
 
@@ -24,7 +23,7 @@ public class TopologyConstants {
     public static final String RECORD_COMPONENT = "DataSource";
     public static final String TICK_COMPONENT = Constants.SYSTEM_COMPONENT_ID;
     public static final String DRPC_COMPONENT = DRPCSpout.class.getSimpleName();
-    public static final String PREPARE_COMPONENT = PrepareRequest.class.getSimpleName();
+    public static final String PREPARE_COMPONENT = PrepareRequestBolt.class.getSimpleName();
     public static final String FILTER_COMPONENT = FilterBolt.class.getSimpleName();
     public static final String JOIN_COMPONENT = JoinBolt.class.getSimpleName();
     public static final String RETURN_COMPONENT = ReturnResults.class.getSimpleName();
@@ -32,7 +31,7 @@ public class TopologyConstants {
     public static final String TICK_STREAM = Constants.SYSTEM_TICK_STREAM_ID;
     public static final String FILTER_STREAM = FilterBolt.FILTER_STREAM;
     public static final String JOIN_STREAM = JoinBolt.JOIN_STREAM;
-    public static final String RETURN_STREAM = PrepareRequest.RETURN_STREAM;
-    public static final String ARGS_STREAM = PrepareRequest.ARGS_STREAM;
-    public static final String ID_STREAM = PrepareRequest.ID_STREAM;
+    public static final String RETURN_STREAM = PrepareRequestBolt.RETURN_STREAM;
+    public static final String ARGS_STREAM = PrepareRequestBolt.ARGS_STREAM;
+    public static final String ID_STREAM = PrepareRequestBolt.ID_STREAM;
 }

src/test/java/com/yahoo/bullet/parsing/AggregationTest.java
@@ -228,7 +228,17 @@ public class AggregationTest {
         List<Error> errors = aggregation.validate().get();
         Assert.assertEquals(errors.size(), 1);
         Assert.assertEquals(errors.get(0), Aggregation.REQUIRES_FIELD_ERROR);
+    }
 
+    @Test
+    public void testFailValidateOnGroupFieldsAndOperationsMissing() {
+        Aggregation aggregation = new Aggregation();
+        aggregation.setType(GROUP);
+        aggregation.configure(emptyMap());
+
+        List<Error> errors = aggregation.validate().get();
+        Assert.assertEquals(errors.size(), 1);
+        Assert.assertEquals(errors.get(0), Aggregation.REQUIRES_FIELD_OR_OPERATION_ERROR);
     }
 
     @Test

src/test/java/com/yahoo/bullet/storm/PrepareRequestBoltTest.java
@@ -1,0 +1,58 @@
+package com.yahoo.bullet.storm;
+
+import backtype.storm.tuple.Fields;
+import backtype.storm.tuple.Tuple;
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import static com.yahoo.bullet.storm.PrepareRequestBolt.ARGS_FIELD;
+import static com.yahoo.bullet.storm.PrepareRequestBolt.ARGS_STREAM;
+import static com.yahoo.bullet.storm.PrepareRequestBolt.ID_STREAM;
+import static com.yahoo.bullet.storm.PrepareRequestBolt.REQUEST_FIELD;
+import static com.yahoo.bullet.storm.PrepareRequestBolt.RETURN_FIELD;
+import static com.yahoo.bullet.storm.PrepareRequestBolt.RETURN_STREAM;
+
+public class PrepareRequestBoltTest {
+    private PrepareRequestBolt bolt;
+    private CustomCollector collector;
+
+    @BeforeMethod
+    public void setup() {
+        collector = new CustomCollector();
+        bolt = ComponentUtils.prepare(new PrepareRequestBolt(), collector);
+    }
+
+    @Test
+    public void testOutputFields() {
+        CustomOutputFieldsDeclarer declarer = new CustomOutputFieldsDeclarer();
+        bolt.declareOutputFields(declarer);
+        Fields expectedArgsFields = new Fields(REQUEST_FIELD, ARGS_FIELD);
+        Fields expectedReturnFields = new Fields(REQUEST_FIELD, RETURN_FIELD);
+        Fields expectedIDFields = new Fields(REQUEST_FIELD);
+        Assert.assertTrue(declarer.areFieldsPresent(ARGS_STREAM, false, expectedArgsFields));
+        Assert.assertTrue(declarer.areFieldsPresent(RETURN_STREAM, false, expectedReturnFields));
+        Assert.assertTrue(declarer.areFieldsPresent(ID_STREAM, false, expectedIDFields));
+    }
+
+    @Test
+    public void testExecution() {
+        String requestBody = "fake drpc request";
+        String returnInfo = "fake return info";
+
+        Tuple sample = TupleUtils.makeTuple(requestBody, returnInfo);
+        bolt.execute(sample);
+
+        Assert.assertEquals(collector.getEmittedCount(), 3);
+        Assert.assertTrue(collector.wasAcked(sample));
+
+        Object generatedID = collector.getMthElementFromNthTupleEmittedTo(ARGS_STREAM, 1, 0).get();
+        Long longID = (Long) generatedID;
+
+        Assert.assertEquals(collector.getMthElementFromNthTupleEmittedTo(ID_STREAM, 1, 0).get(), longID);
+        Assert.assertEquals(collector.getMthElementFromNthTupleEmittedTo(RETURN_STREAM, 1, 0).get(), longID);
+
+        Assert.assertEquals(collector.getMthElementFromNthTupleEmittedTo(ARGS_STREAM, 1, 1).get(), requestBody);
+        Assert.assertEquals(collector.getMthElementFromNthTupleEmittedTo(RETURN_STREAM, 1, 1).get(), returnInfo);
+    }
+}